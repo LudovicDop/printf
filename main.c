2bfe508e5dc23a7222a43f3d5766209a530ad97d
Loacking <LudovicDop@gmail.com>
2023-11-15 10:35:06 +0100

    in progress

--- main.c
+++ main.c
@@ -1,11 +1,28 @@
-#include "./libft/libft.h"
-#include <stdio.h>
+#include "ft_printf.h"
+
+int	sum(int count, ...)
+{
+	va_list	args;
+	va_start(args, count);
+	
+	int	s;
+	int	x;
+	
+	x = va_arg(args, int);	
+	s = 0;
+	while (x)
+	{
+		s += x;
+		x = va_arg(args, int);
+	}
+	va_end(args);
+	return(s);
+}
 
 int	main(void)
 {
 	printf("Hello World!\n");
-	int	*test;
-	
-	test = ft_calloc(1, 1);
+	printf("Sum = %d\n",sum(3, 1, 2, 3, NULL));
+	ft_printf("Hello World");
 	return (0);
 }
